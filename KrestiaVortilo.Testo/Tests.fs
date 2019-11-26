201de162760251b1c8ef23ba860cdbfab3e12c0f
Sam Wang <samofcorinth@gmx.ca>
2019-11-25 23:05:28 -0800

    Aldonis plurajn testojn

---
+++ KrestiaVortilo.Testo/Tests.fs
@@ -0,0 +1,27 @@
+﻿namespace KrestiaVortilo.Testo
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+open KrestiaVortilo.Vorttipoj
+
+[<TestClass>]
+type VorttipoTestoj () =
+
+   [<TestMethod>]
+   member this.VorttipojTesto () =
+      [ ("kresku", NombrigeblaKlaso, Infinitivo)
+        ("kreski", NombrigeblaKlaso, NekonitaNombro)
+        ("gremu", NenombrigeblaKlaso, Infinitivo)
+        ("lugrismaa", NenombrigeblaKlaso, Infinitivo)
+        ("lugrismava", NenombrigeblaKlaso, Ĝerundo) ]
+      |> List.map (fun (vorto, tipo, inflekcio) -> this.KontroliFormon(vorto, tipo, inflekcio))
+      |> ignore
+
+   member _.KontroliFormon (vorto: string, pravaTipo: Vorttipo, pravaInflekcio: Inflekcio) =
+      kontroli vorto
+      |> Option.map
+         (fun (tipo, formo) ->
+            Assert.AreEqual(pravaTipo, tipo)
+            Assert.AreEqual(pravaInflekcio, formo))
+      |> Option.defaultWith (fun () -> Assert.Fail(sprintf "%s estas nevalida" vorto))
---
+++ KrestiaVortilo/Vorttipo.fs
@@ -0,0 +1,56 @@
+﻿namespace KrestiaVortilo
+
+module Vorttipoj =
+   type Inflekcio =
+   | Infinitivo
+   | NekonitaNombro
+   | UnuNombro
+   | PluraNombro
+   | Progresivo
+   | Perfekto
+   | PredikativoEsti
+   | AtributativoEsti
+   | PredikativoHavi
+   | AtributativoHavi
+   | Patiento
+   | Aganto
+   | Translativo
+   | Ĝerundo
+
+   type Vorttipo =
+   | NombrigeblaKlaso
+   | NenombrigeblaKlaso
+   | TransitivaVerbo
+   | NetransitivaVerbo
+   | Priskribanto
+   | Lokokupilo
+
+   type Vortformo = Vorttipo * Inflekcio
+   type VorttipoKontrolilo = (string -> bool)
+
+   let kontrolilaro: (Vortformo * VorttipoKontrolilo) list = [
+      ((NombrigeblaKlaso, Infinitivo),
+         fun s ->
+            [ "pu"; "po"; "paa"; "tu"; "to"; "taa"; "ku"; "ko"; "kaa"]
+            |> List.exists (fun finaĵo -> s.EndsWith(finaĵo)))
+      ((NombrigeblaKlaso, NekonitaNombro),
+         fun s ->
+            [ "pi"; "pe"; "pa"; "ti"; "te"; "ta"; "ki"; "ke"; "ka"]
+            |> List.exists (fun finaĵo -> s.EndsWith(finaĵo)))
+
+      ((NenombrigeblaKlaso, Infinitivo),
+         fun s ->
+            [ "mu"; "mo"; "maa"; "nu"; "no"; "naa"]
+            |> List.exists (fun finaĵo -> s.EndsWith(finaĵo)))
+
+      ((NenombrigeblaKlaso, Ĝerundo),
+         fun s ->
+            [ "miva"; "meva"; "mava"; "niva"; "neva"; "nava"]
+            |> List.exists (fun finaĵo -> s.EndsWith(finaĵo)))
+   ]
+
+   let kontroli (vorto: string): Vortformo option =
+      kontrolilaro
+      |> List.tryFind (fun (_, kontrolilo) -> (kontrolilo vorto))
+      |> Option.map (fun (formo, _) -> formo)
+      
