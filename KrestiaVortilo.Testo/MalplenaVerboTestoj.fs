808319a354888e051e963268d12cc69c194cb593
Sam Wang <samofcorinth@gmx.ca>
2020-01-14 22:51:35 -0800

    Aldonis testojn por malplena verbo

---
+++ KrestiaVortilo.Testo/MalplenaVerboTestoj.fs
@@ -0,0 +1,20 @@
+﻿namespace KrestiaVortilo.Testo
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+open KrestiaVortilo.Vorttipo
+open Testiloj
+
+[<TestClass>]
+type MalplenaVerboTestoj () =
+
+   [<TestMethod>]
+   member _.KontroliTesto () =
+      [ ("meratrem", Infinitivo)
+        ("meratremia", Progresivo)
+        ("meratremio", Perfekto)
+        ("meratremela", Estonteco)
+        ("meratremilim", Translativo)
+        ("meratremena", Ĝerundo) ]
+      |> List.map (fun (vorto, inflekcio) -> kontroliFormon vorto MalplenaVerbo inflekcio)
+      |> ignore
