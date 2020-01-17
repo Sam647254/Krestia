88cac1df397353b29da6ce92a29676a486a2c907
Sam Wang <samofcorinth@gmx.ca>
2020-01-16 22:44:50 -0800

    Parta netransitiva verbo WIP

--- KrestiaVortilo/PartaNetransitivaVerboTraktiloj.fs
+++ KrestiaVortilo/PartaNetransitivaVerboTraktiloj.fs
@@ -3,4 +3,25 @@
 open Vorttipo
 
 module PartaNetransitivaVerboTraktiloj =
-   let traktiloj: Vorttraktilo list = []
+   let normaligi (infinitivo: string) =
+      match infinitivo with
+      | _ when infinitivo.EndsWith("en") -> (NetransitivaVerbo1, PartaNominativo)
+      | _ -> (PartaNetransitivaVerbo, Infinitivo)
+
+   let traktiloj: Vorttraktilo list = [
+      { Kontroli = fun vorto ->
+           if vorto.EndsWith("n")
+           then Some (PartaNetransitivaVerbo, Infinitivo)
+           else None
+        Inflekti = neinflektebla
+        Malinflekti = fun vorto -> (vorto, (PartaNetransitivaVerbo, Infinitivo)) }
+
+      { Kontroli = fun vorto ->
+           if vorto.EndsWith("nia")
+           then Some (PartaNetransitivaVerbo, Progresivo)
+           else None
+        Inflekti = neinflektebla
+        Malinflekti = fun vorto ->
+           let malinflektita = vorto.Substring(0, vorto.Length - 2)
+           (malinflektita, normaligi malinflektita) }
+   ]
