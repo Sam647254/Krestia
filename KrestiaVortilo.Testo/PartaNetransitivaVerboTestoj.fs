1195caef302eddfb3e45fb76e70518c7f7cbb34b
Sam Wang <samofcorinth@gmx.ca>
2020-01-23 23:17:17 -0800

    Liberordaj legiloj WIP

---
+++ KrestiaVortilo.Testo/PartaNetransitivaVerboTestoj.fs
@@ -0,0 +1,21 @@
+﻿namespace KrestiaVortilo.Testo
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+open KrestiaVortilo.Vorttipo
+open Testiloj
+
+[<TestClass>]
+type PartaNetransitivaVerboTestoj () =
+
+   [<TestMethod>]
+   member _.KontroliTesto () =
+      [ ("omon", Infinitivo)
+        ("omonia", Progresivo)
+        ("omonio", Perfekto)
+        ("omonela", Estonteco)
+        ("omoneri", DativoVolo)
+        ("omonema", Ĝerundo)
+        ("omonom", PartaDativo) ]
+      |> List.map (fun (vorto, inflekcio) -> kontroliFormon vorto PartaNetransitivaVerbo inflekcio)
+      |> ignore
