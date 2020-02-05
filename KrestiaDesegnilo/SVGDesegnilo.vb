c159442c07dada5dd758e328a7ba45abcb713272
Sam Wang <samofcorinth@gmx.ca>
2020-02-04 22:07:02 -0800

    Timeran desegnilo WIP

--- KrestiaDesegnilo/SVGDesegnilo.vb
+++ KrestiaDesegnilo/SVGDesegnilo.vb
@@ -2,12 +2,20 @@
 
 Public Class SVGDesegnilo
    Private ReadOnly xmlSkribilo As XmlWriter
+   Private ReadOnly alteco As Integer
+   Private ReadOnly larĝeco As Integer
+   Private ReadOnly documentoLarĝeco As Integer
 
-   Public Sub New(elirejo As String)
+   Public Sub New(elirejo As String, alteco As Integer, larĝeco As Integer,
+                  dokumentoLarĝeco As Integer)
       xmlSkribilo = XmlWriter.Create(elirejo)
+      xmlSkribilo.WriteStartDocument()
+      Me.alteco = alteco
+      Me.larĝeco = larĝeco
    End Sub
 
    Public Sub Fini()
+      xmlSkribilo.WriteEndDocument()
       xmlSkribilo.Close()
    End Sub
 End Class
