bcbeb95bc41cfd86e2010cd2022cd7469d08f750
Sam Wang <samofcorinth@gmx.ca>
2020-02-04 22:03:32 -0800

    Timeran desegnilo WIP

---
+++ KrestiaDesegnilo/SVGDesegnilo.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Xml
+
+Public Class SVGDesegnilo
+   Private ReadOnly xmlSkribilo As XmlWriter
+
+   Public Sub New(elirejo As String)
+      xmlSkribilo = XmlWriter.Create(elirejo)
+   End Sub
+
+   Public Sub Fini()
+      xmlSkribilo.Close()
+   End Sub
+End Class
