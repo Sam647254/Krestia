28d93ed040ff342692da76954f83d87a32eed5e4
Sam Wang <samofcorinth@gmx.ca>
2020-04-15 23:17:17 -0700

    Aldonis RegionEndpoint

--- KrestiaServilo/Services/AwsBlissimbolaro.cs
+++ KrestiaServilo/Services/AwsBlissimbolaro.cs
@@ -1,11 +1,12 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
+using Amazon;
 using Amazon.S3;
 
 namespace KrestiaServilo.Services {
    public class AwsBlissimbolaro : IBlissFonto {
       private const string Bucket = "blissimboloj";
-      private readonly AmazonS3Client _amazonS3Client = new AmazonS3Client();
+      private readonly AmazonS3Client _amazonS3Client = new AmazonS3Client(RegionEndpoint.USWest2);
 
       public async Task<string> AlportiBlissimbolon(int id) {
          using var respondo = await _amazonS3Client.GetObjectAsync(Bucket, $"bliss_svg_id/{id}.svg");
