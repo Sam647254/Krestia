dbaf3cf91e6cbd327fb08710cc68d07a72dc0aea
Sam Wang <smwn@hey.com>
2020-06-26 23:17:17 -0700

    Aldonis finpunkton por la legilo

---
+++ KrestiaServilo/Controllers/LegiloController.cs
@@ -0,0 +1,18 @@
+﻿using KrestiaVortilo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KrestiaServilo.Controllers {
+   [ApiController]
+   [Route("api")]
+   public class LegiloController : ControllerBase {
+      [HttpPost("legi")]
+      public IActionResult Legi([FromBody] string eniro) {
+         var rezulto = Sintaksanalizilo2.analizi(eniro, false);
+         if (rezulto.IsOk) {
+            return Ok(rezulto.ResultValue);
+         }
+
+         return UnprocessableEntity(rezulto.ErrorValue);
+      }
+   }
+}
