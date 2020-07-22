573a6e6ec9c29b3b870c52bd72963e6453824cf5
Sam Wang <smwn@hey.com>
2020-07-21 23:17:17 -0700

    ImperativaLegilo WIP

--- KrestiaVortilo/ImperitivaLegilo.fs
+++ KrestiaVortilo/ImperitivaLegilo.fs
@@ -4,7 +4,7 @@ open System.Collections.Generic
 open KrestiaVortilo.Malinflektado
 open KrestiaVortilo.Sintaksanalizilo2
 
-module Imperativa =   
+module Imperativa =
    type Vorto() =
       class
       end
@@ -12,29 +12,30 @@ module Imperativa =
    type ModifeblaVorto() =
       inherit Vorto()
       member private _.modifantoj = HashSet<Modifanto>()
-      
-      member this.AldoniModifanton(modifanto: Modifanto) =
-         this.modifantoj.Add(modifanto) |> ignore
+
+      member this.AldoniModifanton(modifanto: Modifanto) = this.modifantoj.Add(modifanto) |> ignore
 
    and Argumento(kapo: MalinflektitaVorto) =
       inherit ModifeblaVorto()
-   
+
    and Rezulto =
-      { Frazoj : Predikato list
+      { Frazoj: Predikato list
         Argumentoj: Argumento list }
-   
+
    and Modifanto =
+      | Pridiranto of MalinflektitaVorto
       | EcoDe of Argumento
 
    type ImperitivaLegilo(enira: Queue<MalinflektitaVorto>) =
       let argumentoj = LinkedList<Argumento>()
       let frazoj = List<Predikato>()
+      let atendantaArgumenton = Queue<Argumento -> Argumento>()
 
       member this.Legi(): Result<Rezulto, Eraro> =
          this.LegiSekvan()
          |> Result.map (fun () ->
-            { Frazoj = frazoj |> List.ofSeq
-              Argumentoj = argumentoj |> List.ofSeq })
+               { Frazoj = frazoj |> List.ofSeq
+                 Argumentoj = argumentoj |> List.ofSeq })
 
       member this.LegiSekvan(): Result<unit, Eraro> =
          if enira.Count > 0 then
@@ -52,12 +53,22 @@ module Imperativa =
                            eco.AldoniModifanton(EcoDe(de))
                            argumentoj.AddLast(eco) |> ignore
                            this.LegiSekvan()))
+            elif ĉuAntaŭModifantaVorto sekva then
+               let pridiranto = Pridiranto(sekva)
+               atendantaArgumenton.Enqueue(fun argumento ->
+                  argumento.AldoniModifanton(pridiranto)
+                  argumento)
+               this.LegiSekvan()
+            elif ĉuMalantaŭModifantaVorto sekva then
+               let pridiranto = Pridiranto(sekva)
+               argumentoj.Last.Value.AldoniModifanton(pridiranto)
+               this.LegiSekvan()
             else
                Eraro(sekva.OriginalaVorto, sprintf "Can't parse %s" sekva.OriginalaVorto.Vorto)
                |> Error
          else
             Ok()
-            
+
       member private this.PostuliArgumenton(): Result<Argumento, Eraro> =
          if argumentoj.Count > 0 then
             let argumento = argumentoj.First.Value
@@ -68,9 +79,12 @@ module Imperativa =
 
       member private this.LegiArgumenton(): Result<Argumento, Eraro> =
          let argumento = enira.Dequeue()
-         if not (ĉuArgumentaVorto argumento)
-         then Error
-                 (Eraro
-                    (argumento.OriginalaVorto, sprintf "%s is not a valid argument" argumento.OriginalaVorto.Vorto))
+         if not (ĉuArgumentaVorto argumento) then
+            Error(Eraro(argumento.OriginalaVorto, sprintf "%s is not a valid argument" argumento.OriginalaVorto.Vorto))
          else
-            Ok(Argumento(argumento))
+            let novaArgumento =
+               atendantaArgumenton
+               |> Seq.fold (fun ak sekva -> sekva ak) (Argumento(argumento))
+
+            atendantaArgumenton.Clear()
+            novaArgumento |> Ok
