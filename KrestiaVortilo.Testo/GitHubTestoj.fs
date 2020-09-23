9c98b361d7ba203ff6fb8db2380433110f7baf76
Sam Wang <samofcorinth@gmx.ca>
2020-09-22 23:17:17 -0700

    Korektis la valencon de Imperativo kaj Hortativo

---
+++ KrestiaVortilo.Testo/GitHubTestoj.fs
@@ -0,0 +1,17 @@
+﻿namespace KrestiaVortilo.Testo
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open KrestiaVortilo.Sintaksanalizilo2
+open Testiloj
+
+[<TestClass>]
+type GitHubTestoj() =
+   
+   [<TestMethod>]
+   // https://github.com/Sam647254/Krestia/issues/1
+   member _.``Parser can't parse "liverasea linetiga"``() =
+      let [ liverasea; linetiga ] = [ "liverasea"; "linetiga" ] |> List.map praveMalinflekti
+      kontroliFrazojn
+         "liverasea linetiga"
+         [ { Kapo = verbo liverasea [ pridiranto linetiga ]
+             Argumentoj = [] } ]
