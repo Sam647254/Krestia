bff1e4f98c9c5e8b0a5c74c4b7353a41dfc604e3
Sam Wang <samofcorinth@gmx.ca>
2021-02-24 22:53:25 -0800

    Korektis la nombrajn testojn

--- KrestiaVortilo.Testo/NombroTestoj.fs
+++ KrestiaVortilo.Testo/NombroTestoj.fs
@@ -7,22 +7,22 @@ open Testiloj
 type NombroTestoj() =
    [<TestMethod>]
    member _.NaturajNombroj() =
-      kontroliKalkulon "sona" 7m
-      kontroliKalkulon "po mi vo nona" 1023m
-      kontroliKalkulon "te si li sona" 4567m
-      kontroliKalkulon "ke gi po mira" 8910m
+      kontroliKalkulon "sona" 7.0
+      kontroliKalkulon "po mi vo nona" 1023.0
+      kontroliKalkulon "te si li sona" 4567.0
+      kontroliKalkulon "ke gi po mira" 8910.0
    
    [<TestMethod>]
    member _.Kalkulado() =
-      kontroliKalkulon "sona tikal nona" 10m
-      kontroliKalkulon "sona senal nona" 4m
-      kontroliKalkulon "sona petal nona" 21m
-      kontroliKalkulon "sona visal nona" (7m/3m)
+      kontroliKalkulon "sona tikal nona" 10.0
+      kontroliKalkulon "sona senal nona" 4.0
+      kontroliKalkulon "sona petal nona" 21.0
+      kontroliKalkulon "sona visal nona" (7.0/3.0)
 
    [<TestMethod>]
    member _.Kalkulado2() =
-      kontroliKalkulon "sona tikal nona tikal vora" 12m
-      kontroliKalkulon "sona tikal nona nil tikal vora" 12m
-      kontroliKalkulon "sona petal nona nil tikal nona" 24m
-      kontroliKalkulon "sona petal nona tikal nona" 42m
+      kontroliKalkulon "sona tikal nona tikal vora" 12.0
+      kontroliKalkulon "sona tikal nona nil tikal vora" 12.0
+      kontroliKalkulon "sona petal nona nil tikal nona" 24.0
+      kontroliKalkulon "sona petal nona tikal nona" 42.0
       
