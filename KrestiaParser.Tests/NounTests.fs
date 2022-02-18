bf1b365c42cd00ffa39974c899cbc189e0f80700
Sam Wang <samofcorinth@gmx.ca>
2022-02-17 20:44:43 -0800

    Added more unit tests

--- KrestiaParser.Tests/NounTests.fs
+++ KrestiaParser.Tests/NounTests.fs
@@ -7,64 +7,111 @@ open KrestiaParser.WordType
 open TestUtils
 
 let private countableNouns =
-   [ "vilipi"
-     "brepe"
-     "moropa"
-     "kluti"
-     "tote"
-     "revita"
-     "meki"
-     "grike"
-     "lurika" ]
+    [ "vilipi"
+      "brepe"
+      "moropa"
+      "kluti"
+      "tote"
+      "revita"
+      "meki"
+      "grike"
+      "lurika" ]
 
 let private uncountableNouns =
-   [ "gremi"
-     "rinome"
-     "luvema"
-     "lini"
-     "kresne"
-     "duna" ]
+    [ "gremi"
+      "rinome"
+      "luvema"
+      "lini"
+      "kresne"
+      "duna" ]
 
 let private countablePIs =
-   [ "vilipu"
-     "brepo"
-     "moropaa"
-     "klutu"
-     "toto"
-     "revitaa"
-     "meku"
-     "griko"
-     "lurikaa" ]
+    [ "vilipu"
+      "brepo"
+      "moropaa"
+      "klutu"
+      "toto"
+      "revitaa"
+      "meku"
+      "griko"
+      "lurikaa" ]
+
+let private uncountablePIs =
+    [ "gremu"
+      "rinomo"
+      "luvemaa"
+      "linu"
+      "kresno"
+      "dunaa" ]
 
 [<TestFixture>]
 [<Parallelizable(ParallelScope.All)>]
 type NounTests() =
 
-   [<Test>]
-   member this.``Can recognize base countable nouns``() =
-      this.CheckBaseWord CountableNoun countableNouns
+    [<Test>]
+    member this.``Can recognize base countable nouns``() =
+        this.CheckBaseWord CountableNoun countableNouns
 
-   [<Test>]
-   member this.``Can recognize base uncountable nouns``() =
-      this.CheckBaseWord UncountableNoun uncountableNouns
-      
-   [<Test>]
-   member this.``Can recognize predicative identity``() =
-      this.CheckSingleInflection CountableNoun PredicativeIdentity countablePIs
+    [<Test>]
+    member this.``Can recognize base uncountable nouns``() =
+        this.CheckBaseWord UncountableNoun uncountableNouns
 
-   member private _.CheckBaseWord expectedType words =
-      for word in words do
-         optionTest $"Could not decompose %s{word}" {
-            let! result = decompose word
-            Assert.IsEmpty(result.steps, $"%s{word} should not have any inflection steps")
-            Assert.AreEqual(expectedType, result.baseType, $"%s{word} should be of type %O{expectedType}")
-            Assert.AreEqual(word, result.baseWord, $"%s{word} should not have additional suffixes")
-         }
+    [<Test>]
+    member this.``Can recognize predicative identity (countable)``() =
+        this.CheckSingleInflection CountableNoun PredicativeIdentity countablePIs
 
-   member private _.CheckSingleInflection expectedType (expectedInflection: Inflection) words =
-      for word in words do
-         optionTest $"Could not decompose %s{word}" {
-            let! result = decompose word
-            Assert.AreEqual(expectedType, result.baseType, $"%s{word} should be of type %O{expectedType}")
-            Assert.AreEqual([expectedInflection], result.steps)
-         }
+    [<Test>]
+    member this.``Can recognize predicative identity (uncountable)``() =
+        this.CheckSingleInflection UncountableNoun PredicativeIdentity uncountablePIs
+
+    [<Test>]
+    member this.``Can recognize possession (countable)``() =
+        this.CheckSingleSuffix CountableNoun Possession countableNouns "nsa"
+    
+    [<Test>]
+    member this.``Can recognize possession (uncountable)``() =
+        this.CheckSingleSuffix UncountableNoun Possession uncountableNouns "nsa"
+    
+    [<Test>]
+    member this.``Can recognize AI (countable)``() =
+        this.CheckSingleSuffix CountableNoun AttributiveIdentityPostfix countableNouns "ga"
+        this.CheckSingleSuffix CountableNoun AttributiveIdentityPrefix countableNouns "va"
+    
+    [<Test>]
+    member this.``Can recognize AI (uncountable)``() =
+        this.CheckSingleSuffix UncountableNoun AttributiveIdentityPostfix uncountableNouns "ga"
+        this.CheckSingleSuffix UncountableNoun AttributiveIdentityPrefix uncountableNouns "va"
+
+    member private _.CheckBaseWord expectedType words =
+        for word in words do
+            optionTest $"Could not decompose %s{word}" {
+                let! result = decompose word
+                Assert.IsEmpty(result.steps, $"%s{word} should not have any inflection steps")
+                Assert.AreEqual(expectedType, result.baseType, $"%s{word} should be of type %O{expectedType}")
+                Assert.AreEqual(word, result.baseWord, $"%s{word} should not have additional suffixes")
+            }
+
+    member private _.CheckSingleInflection expectedType (expectedInflection: Inflection) words =
+        for word in words do
+            optionTest $"Could not decompose %s{word}" {
+                let! result = decompose word
+                Assert.AreEqual(expectedType, result.baseType, $"%s{word} should be of type %O{expectedType}")
+                Assert.AreEqual([ expectedInflection ], result.steps)
+            }
+
+    member private _.CheckSingleSuffix expectedType expectedInflection words suffix =
+        for word in words do
+            let suffixed = word + suffix
+
+            optionTest $"Could not decompose %s{suffixed}" {
+                let! result = decompose suffixed
+                Assert.AreEqual(expectedType, result.baseType, $"%s{word} should be of type %O{expectedType}")
+
+                Assert.AreEqual(
+                    [ expectedInflection ],
+                    result.steps,
+                    $"%s{word} should have inflection %O{expectedInflection}"
+                )
+
+                Assert.AreEqual(word, result.baseWord)
+            }
