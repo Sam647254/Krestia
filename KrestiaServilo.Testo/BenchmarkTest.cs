0d8b6fe41c05ce2692da5bd871b5fb4bf2b1ed16
Sam Wang <samofcorinth@gmx.ca>
2022-05-18 20:57:39 -0700

    Added KrestiaClient

---
+++ KrestiaServilo.Testo/BenchmarkTest.cs
@@ -0,0 +1,12 @@
+﻿using KrestiaServilo.Services;
+using NUnit.Framework;
+
+namespace KrestiaServilo.Testo; 
+
+public class BenchmarkTest {
+   [Test]
+   public void BenchmarkDictionaryLoad() {
+      var _ = new VortaroService();
+      Assert.Pass();
+   }
+}
--- KrestiaServilo/Startup.cs
+++ KrestiaServilo/Startup.cs
@@ -6,38 +6,41 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace KrestiaServilo {
-   public class Startup {
-      public Startup(IConfiguration configuration) {
-         Configuration = configuration;
+namespace KrestiaServilo; 
+
+public class Startup {
+   public Startup(IConfiguration configuration) {
+      Configuration = configuration;
+   }
+
+   public IConfiguration Configuration { get; }
+
+   // This method gets called by the runtime. Use this method to add services to the container.
+   public void ConfigureServices(IServiceCollection services) {
+      services.AddControllersWithViews().AddJsonOptions(options => {
+         options.JsonSerializerOptions.Converters.Add(new ModifantoJsonConverter());
+         options.JsonSerializerOptions.Converters.Add(new ArgumentoJsonConverter());
+         options.JsonSerializerOptions.Converters.Add(new MalinflektaŜtupoJsonConverter());
+      });
+      services.AddSingleton<IVortaroService>(_ => new VortaroService());
+      services.AddSpaStaticFiles(configuration => {
+         configuration.RootPath = "ClientApp/build";
+      });
+   }
+
+   // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+   public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+      if (env.IsDevelopment()) {
+         app.UseWebAssemblyDebugging();
       }
 
-      public IConfiguration Configuration { get; }
-
-      // This method gets called by the runtime. Use this method to add services to the container.
-      public void ConfigureServices(IServiceCollection services) {
-         services.AddControllersWithViews().AddJsonOptions(options => {
-            options.JsonSerializerOptions.Converters.Add(new ModifantoJsonConverter());
-            options.JsonSerializerOptions.Converters.Add(new ArgumentoJsonConverter());
-            options.JsonSerializerOptions.Converters.Add(new MalinflektaŜtupoJsonConverter());
-         });
-         services.AddSingleton<IVortaroService>(_ => new VortaroService());
-         services.AddSpaStaticFiles(configuration => {
-            configuration.RootPath = "ClientApp/build";
-         });
-      }
-
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-         if (env.IsDevelopment()) {
-            app.UseDeveloperExceptionPage();
-         }
-
-         app.UseHttpsRedirection();
-         app.UseStaticFiles();
-         app.UseSpaStaticFiles();
-         app.UseRouting();
-         app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-      }
+      app.UseHttpsRedirection();
+      app.UseBlazorFrameworkFiles();
+      app.UseStaticFiles();
+      app.UseRouting();
+      app.UseEndpoints(endpoints => {
+         endpoints.MapControllers();
+         endpoints.MapFallbackToFile("index.html");
+      });
    }
 }
